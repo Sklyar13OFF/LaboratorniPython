06baff9bb0aa5e44cea8c0b98dcb25c20d288367
Daniel Sklyuarov <dsklyarov8@gmail.com>
2021-12-26 22:18:14 +0200

    r

--- Labkaseven/exer5.py
+++ Labkaseven/exer5.py
@@ -1,1 +1,14 @@
- 
+'''
+s -  кількість рядків матриці
+l - кількість стовпців матриці
+a - матриця
+'''
+s = int(input('Введіть кількість рядків матриці:  '))
+l = int(input('Введіть кількість стовпців матриці:  '))
+a = [[int(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn,n))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
+
+a = list(zip(*sorted(list(zip(*a)),  #сортування матриці
+                          key=lambda column: sum(abs(el) for el in column
+                                                 if el < 0 and el % 2))))
+
+print(*a, sep="\n")
--- Labkaseven/exer6.py
+++ Labkaseven/exer6.py
@@ -1,1 +1,18 @@
- 
+'''
+s -  кількість рядків матриці
+l - кількість стовпців матриці
+a - матриця
+suma - сума
+sum_neg - функція знаходженння суми
+'''
+s = int(input('Введіть кількість рядків матриці:  '))
+l = int(input('Введіть кількість стовпців матриці:  '))
+a = [[int(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn,n))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
+def sum_neg(matrix) : #функція
+    suma = 0
+    for i in range(len(matrix)) :
+        for j in range(len(matrix[i])) :
+            if matrix[i][j] < 0 and i == j:
+                suma += matrix[i][j]
+    return suma
+print('Сума:  ',sum_neg(a))
