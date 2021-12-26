755734e38eafec38153ee266072b80d23ce482e8
Daniel Sklyuarov <dsklyarov8@gmail.com>
2021-12-26 22:17:13 +0200

    s

--- Labkaeight/exer3.py
+++ Labkaeight/exer3.py
@@ -3,13 +3,13 @@ n - кількість доданків
 recurssion - рекурсивна функція
 result - результат
 """
-n = int(input('Введіть кількість доданків :  '))
+n = int(input('Введіть кількість доданків :  '))   #введення кількості доданків
 def  recurssion(n):    #рекурсія
     if n == 0:
         return 0
     elif n == 1 or n == 2:
         return 9
     else:
-        return recurssion(n-1) + 4*recurssion(n-3)
+        return recurssion(n-1) + 4*recurssion(n-3)    #обчислення рекурсії
 result = recurssion(n)  #результат
 print('Результатом буде : {0}'.format(result))
