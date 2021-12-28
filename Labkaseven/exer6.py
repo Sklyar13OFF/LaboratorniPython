fc4175c71d7301958d4fe90844bdb5c530430bee
Daniel Sklyarov <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 21:02:02 +0200

    Update exer6.py

--- Labkaseven/exer6.py
+++ Labkaseven/exer6.py
@@ -2,13 +2,11 @@
 s -  кількість рядків матриці
 l - кількість стовпців матриці
 a - матриця
-suma - сума
-sum_neg - функція знаходженння суми
 '''
 s = int(input('Введіть кількість рядків матриці:  '))
 l = int(input('Введіть кількість стовпців матриці:  '))
-a = [[int(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn,n))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
-def sum_neg(matrix) : #функція
+a = [[float(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn,n))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
+def sum_neg(matrix) :
     suma = 0
     for i in range(len(matrix)) :
         for j in range(len(matrix[i])) :
