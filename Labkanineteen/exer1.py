dd94b074c1f2384d9a690d814645175c23d18478
Sklyar13OFF <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 18:43:24 +0200

    Update exer1.py

--- Labkanineteen/exer1.py
+++ Labkanineteen/exer1.py
@@ -1,10 +1,7 @@
-with open('mynumbers.txt') as file:
-    line = file.readlines()
-    num = []
-    counter = 0
-    for i in line:
-        num.extend(map(int, i.split()))
-    for el in range(len(num)):
-        if type(num[el]) == float or type(num[el]) == int:
-            counter += 1
-    print('Кількість чисел: {0}'.format(counter))
+f = open('mynumbers.txt')
+line = f.readline().split(' ')
+counter = 0
+for i in line:
+    if i.isdigit():
+        counter += 1
+print(counter)
