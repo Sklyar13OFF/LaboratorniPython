9f8990ae535a219afc497a2ea2e070627e9dfb75
Sklyar13OFF <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 18:53:56 +0200

    Update exer1.py

--- Labkanineteen/exer1.py
+++ Labkanineteen/exer1.py
@@ -1,5 +1,10 @@
-f = open('mynumbers.txt')
-line = f.readline().split(' ')
+'''
+f - допоміжна змінна
+line - перша лінія текстового файла
+counter - лічильник
+'''
+f = open('mynumbers.txt') #відкриття файла
+line = f.readline().split(' ') 
 counter = 0
 for i in line:
     if i.isdigit():
