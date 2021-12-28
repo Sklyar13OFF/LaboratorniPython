70fa741ba878485391d790e0a6f462f5dfde901f
Daniel Sklyarov <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 21:01:34 +0200

    Update exer5.py

--- Labkaseven/exer5.py
+++ Labkaseven/exer5.py
@@ -6,7 +6,7 @@ a - матриця
 '''
 s = int(input('Введіть кількість рядків матриці:  '))
 l = int(input('Введіть кількість стовпців матриці:  '))
-a = [[int(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn ,n))) for n in range(l)] for nn in range(s)]  # ввід елементів матриці
+a = [[float(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn ,n))) for n in range(l)] for nn in range(s)]  # ввід елементів матриці
 
 a = list(zip(*sorted(list(zip(*a)),  # сортування матриці
                      key=lambda column: sum(abs(el) for el in column
