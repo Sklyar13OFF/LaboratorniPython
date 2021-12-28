ba5bfc93326a28a61d086b14e7391f21755597fb
Daniel Sklyarov <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 21:01:02 +0200

    Update exer4.py

--- Labkaseven/exer4.py
+++ Labkaseven/exer4.py
@@ -5,7 +5,7 @@ a - матриця
 '''
 s = int(input('Введіть кількість рядків матриці:  '))
 l = int(input('Введіть кількість стовпців матриці:  '))
-a = [[int(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(n,nn))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
+a = [[float(input('Введіть елемент матриці у {0} рядку та у {1} стовпці:  '.format(nn,n))) for n in range(l)] for nn in range(s)] #ввід елементів матриці
 for i in range(0,len(a)):
     if i%2 == 1:
         a[i] = sorted(a[i],reverse = True)
