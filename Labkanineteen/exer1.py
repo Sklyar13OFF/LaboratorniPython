c58cff45df5e8c6c301b37b66ef1fceb5495e7d7
Sklyar13OFF <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 12:35:26 +0200

    Update exer1.py

--- Labkanineteen/exer1.py
+++ Labkanineteen/exer1.py
@@ -1,9 +1,10 @@
-infile = open('mynumbers.txt','r')
-lines = infile.readlines()
-a=[]
-for line in lines:
-    if line < 0:
-        a.append(line)
-for el in a:
-    el = abs(el)
-print(min(a))
+with open('mynumbers.txt') as file:
+    line = file.readlines()
+    num = []
+    counter = 0
+    for i in line:
+        num.extend(map(int, i.split()))
+    for el in range(len(num)):
+        if type(num[el]) == float or type(num[el]) == int:
+            counter += 1
+    print('Кількість чисел: {0}'.format(counter))
