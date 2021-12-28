0ed5f4a9405a86b00478a07c1e691b35ad0329da
Sklyar13OFF <45038310+Sklyar13OFF@users.noreply.github.com>
2021-12-28 18:08:11 +0200

    Update exer1.py

--- Labkaten/exer1.py
+++ Labkaten/exer1.py
@@ -1,29 +1,35 @@
-import numpy
+import numpy as np
 class Line:
     def __init__(self):
-        self.a = float(input('Введіть перший коефіцієнт канонічного рівняння прямої:  '))
-        self.b = float(input('Введіть другий коефіцієнт канонічного рівняння прямої:  '))
-        self.a_1 = float(input('Введіть перший коефіцієнт канонічного рівняння другої прямої:  '))
-        self.b_1 = float(input('Введіть другий коефіцієнт канонічного рівняння другої прямої:  '))
+        self.x_0 = float(input('Введіть першу координату точки перетину першої прямої: '))
+        self.y_0 = float(input('Введіть другу координату точки перетину першої прямої: '))
+        self.m = float(input('Введіть перший коефіцієнт напрямного вектора першої прямої: '))
+        self.n = float(input('Введыть другий коефіцієнт напрямного вектора першої прямої: '))
     def coef(self):
-        return [self.a,self.b]
-    def crossing(self):
-        self.a_1 = float(input('Введіть перший коефіцієнт канонічного рівняння другої прямої:  '))
-        self.b_1 = float(input('Введіть другий коефіцієнт канонічного рівняння другої прямої:  '))
-        self.m1 = numpy.array([[self.a, self.b], [self.a_1, self.b_1]])
-        self.v1 = numpy.array([1,1])
-        return numpy.linalg.solve(self.m1, self.v1)
+        return [self.x_0,self.y_0,self.m,self.n]
+    def intersection(self):
+        self.x_1 = float(input('Введіть першу ккординату точки перетину другої прямої: '))
+        self.y_1 = float(input('Введіть другу координату точки перетину другої прямої: '))
+        self.j = float(input('Введіть перший коефіцієнт напрямного вектора другої прямої: '))
+        self.k = float(input('Введыть другий коефіцієнт напрямного вектора другої прямої: '))
+        self.A = np.array([[self.m,-self.j],[self.n,-self.k]])
+        self.Res = np.array([-self.x_0+self.x_1,-self.y_0+self.y_1])
+        self.dot = np.linalg.solve(self.A,self.Res)
+        return self.dot
     def paralel(self):
-        if self.a*self.a_1 + self.b*self.b_1 == 0:
-            return ("Паралельні")
+        if self.m*self.j + self.n*self.k == 0:
+            return 'Паралельні'
         else:
-            return ("Не паралельні")
-    def belong(self):
-        self.x = float(input('Введіть першу координату точки :  '))
-        self.y = float(input('Введіть другу координату точки  :  '))
-        if self.x/ self.a + self.y/self.b == 1:
-            return 'Належить'
+            return 'Не паралельні'
+    def belongs(self):
+        self.dot_x = float(input('Введіть першу координату точки: '))
+        self.dot_y = float(input('Введіть другу координату точки: '))
+        if ((self.dot_x - self.x_0)/self.m) == ((self.dot_y - self.y_0)/self.n):
+            return 'Точка належить 1-ій прямій'
+        elif ((self.dot_x - self.x_1)/self.j) == ((self.dot_y - self.y_1)/self.k):
+            return 'Точка не належить 2-ій прямій'
         else:
-            return 'Не належить'
-A  = Line()
-print(A.paralel())
+            return 'Точка не належить жодній прямій'
+A = Line()
+print(A.intersection())
+print(A.belongs())
